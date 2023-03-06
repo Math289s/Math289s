b8a6f6170b171e2adfd5a6af49aac58f07d459b5
Math289s <39995329+Math289s@users.noreply.github.com>
2023-03-06 08:33:12 +0100

    Add files via upload

---
+++ EksamensProjekt/src/eksamensprojekt/EksamensProjekt.java
@@ -0,0 +1,22 @@
+/*
+ *  © Mathias Hessellund - mhessellund26@gmail.com
+ * License: by-nc-sa 4.0 
+ * Legal: https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ * Human Readable: https://creativecommons.org/licenses/by-nc-nd/4.0/
+ */
+package eksamensprojekt;
+
+/**
+ *
+ * @author Mathias
+ */
+public class EksamensProjekt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
